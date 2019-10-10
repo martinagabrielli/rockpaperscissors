e06998d0caac02fe7560799101626235c52636ca
Martina <martina.gabrielli@yahoo.it>
2019-10-10 15:56:14 +0100

    added script function for game initialisation

---
+++ script.js
@@ -0,0 +1,25 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    // start the game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+
+    // call all the inner functions
+    startGame();
+    updateScore();
+};
+
+// start the game function
+
+game();
+
