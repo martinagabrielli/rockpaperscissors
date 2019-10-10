06232fbd984311b24dca66ce6fe0dd23986f7ec6
Martina <martina.gabrielli@yahoo.it>
2019-10-10 17:19:38 +0100

    added score function and hand compare

--- script.js
+++ script.js
@@ -22,14 +22,78 @@ const game = () => {
         // computer options
         const computerOptions = ['rock', 'paper', 'scissors'];
 
-        options.forEach(option=>{
+        options.forEach(option => {
             option.addEventListener('click', function(){
                 // computer choice
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
-                console.log(computerChoice);
+                // here is where we call 'compare hands'
+                
+                compareHands(this.textContent, computerChoice);
+
+                // update images
+                playerHand.src = `img/${this.textContent}.png`;
+                computerHand.src = `img/${computerChoice}.png`;
             });
         });
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+
+    const compareHands = (playerChoice, computerChoice) => {
+        // update text
+        const winner = document.querySelector('.winner');
+        // checking for a tie
+        if(playerChoice === computerChoice){
+            winner.textContent = 'It\'s a tie';
+            return;
+        }
+        // check for rock
+        if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            } 
+        }
+        // check for paper
+        if(playerChoice === 'paper'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                return;
+            } 
+        }
+        // check for scissors
+        if(playerChoice === 'scissors'){
+            if(computerChoice === 'rock'){
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            } 
+        }
     }
 
     // call all the inner functions
